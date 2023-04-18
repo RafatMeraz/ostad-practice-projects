167caf9a5d0c4fa0382ee2be8404d6ea17abb354
RafatMeraz <razhossain201@gmail.com>
2023-04-18 23:23:36 +0600

    book store application added

---
+++ book_library/bin/book_library.dart
@@ -0,0 +1,5 @@
+import 'package:book_library/book_library.dart' as book_library;
+
+void main(List<String> arguments) {
+  print('Hello world: ${book_library.calculate()}!');
+}
---
+++ book_library/test/book_library_test.dart
@@ -0,0 +1,8 @@
+import 'package:book_library/book_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
