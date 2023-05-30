85a6ebb985036602cfddabea0bfa7615bd0bb2a0
Mahfujur Rahman Musfique <musfique113@gmail.com>
2023-05-31 00:42:15 +0600

    added screen for specfic task

---
+++ h20/lib/presentation/screens/daily_report_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DailyReportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daily Report'),
+      ),
+      body: Center(
+        child: Text('This is the Daily Report Screen'),
+      ),
+    );
+  }
+}
--- h20/lib/presentation/screens/home_screen.dart
+++ h20/lib/presentation/screens/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:h20/presentation/screens/reminder_screen.dart';
+import 'package:h20/presentation/screens/weekly_report_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:h20/models/water_tracker.dart';
+
+import 'daily_report_screen.dart';
+import 'monthly_report_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -28,6 +33,42 @@ class HomeScreen extends StatelessWidget {
                 Provider.of<WaterTracker>(context, listen: false).incrementWaterIntake(250);
               },
               child: Text('Add 250 ml'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DailyReportScreen()),
+                );
+              },
+              child: Text('Daily Report'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => WeeklyReportScreen()),
+                );
+              },
+              child: Text('Weekly Report'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MonthlyReportScreen()),
+                );
+              },
+              child: Text('Monthly Report'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ReminderScreen()),
+                );
+              },
+              child: Text('Set Reminder'),
             ),
           ],
         ),
