5d7ef1ea583a3784b3d8b006da6ab1f53dea9caa
RafatMeraz <razhossain201@gmail.com>
2023-06-04 13:44:42 +0600

    [h20] : splash screen has been added

--- h20/lib/presentation/app.dart
+++ h20/lib/presentation/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:h20/presentation/screens/splash_screen.dart';
+import 'package:h20/presentation/utils/color_utils.dart';
 
 class H2OApp extends StatelessWidget {
   const H2OApp({Key? key}) : super(key: key);
@@ -8,10 +9,12 @@ class H2OApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.light
+        brightness: Brightness.light,
+        primarySwatch: ColorUtils.getMaterialColor(ColorUtils.spanishSkyBlue),
       ),
       darkTheme: ThemeData(
-        brightness: Brightness.dark
+        brightness: Brightness.dark,
+        primarySwatch: ColorUtils.getMaterialColor(ColorUtils.spanishSkyBlue),
       ),
       home: const SplashScreen(),
     );
--- h20/lib/presentation/screens/splash_screen.dart
+++ h20/lib/presentation/screens/splash_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:h20/presentation/utils/color_utils.dart';
+import 'package:lottie/lottie.dart';
 
-class SplashScreen extends StatelessWidget {
+import 'home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Future.delayed(const Duration(seconds: 2)).then((_) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Spacer(),
+          Lottie.asset('assets/lotties/water-animation.json', width: 100),
+          const SizedBox(
+            height: 16,
+          ),
+          Text('Water Reminder',
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                  fontWeight: FontWeight.w400,
+                  color: ColorUtils.spanishSkyBlue)),
+          const Spacer(),
+          const Padding(
+              padding: EdgeInsets.all(16), child: CircularProgressIndicator()),
+          const Row(),
+        ],
+      ),
+    );
   }
 }
